e03b0bedd1a7bb2de41339f944897ec19064d460
Seungho <hamsh2003@naver.com>
2022-12-12 23:17:15 +0900

    버전업 및 공격모션 적용

--- Assets/02.Scripts/Battle/Unit.cs
+++ Assets/02.Scripts/Battle/Unit.cs
@@ -1,6 +1,9 @@
 public interface Unit
 {
-    public void Iattack();
-    public void Idamaged();
+    // 유닛에게 할당되는 유닛 인터페이스.
+    
+    public void I_Attack();
+    public void I_Damaged();
+    
 }
 
